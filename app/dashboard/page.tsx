170aa4b6cb5b3ed6ca5c642210f1708dfc547910
aj2980 <jainabhishek624@gmail.com>
2025-11-04 01:35:30 +0530

    1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+  return <p>Dashboard Page</p>;
+}
+
+
+// This is how you can create different pages in Next.js: create a new route segment using a folder, and add a page file inside it.
